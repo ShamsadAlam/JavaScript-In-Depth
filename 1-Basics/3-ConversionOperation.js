58a4b826c34abe396aed90fdb8d58fcfb8e469ae
SHAMSAD ALAM <77450372+ShamsadAlam@users.noreply.github.com>
2025-03-03 03:14:26 +0000

    operations understanding

--- 1-Basics/3-ConversionOperation.js
+++ 1-Basics/3-ConversionOperation.js
@@ -58,3 +58,13 @@ console.log(1 + "2");
 console.log("1 + 2 + 2")
 console.log("1" + 2 + 2)
 console.log(1 + 2 + "2")
+
+console.log(+true); // -> Allowed
+// console.log(true+); --> Not Allowed
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
