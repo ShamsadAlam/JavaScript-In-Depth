b62f0b1fd0111b37d37f5768810dc9c563beff93
SHAMSAD ALAM <77450372+ShamsadAlam@users.noreply.github.com>
2025-03-03 03:40:31 +0000

    DataTypes and Comparisons

--- 1-Basics/2-DataTypes.js
+++ 1-Basics/2-DataTypes.js
@@ -3,17 +3,37 @@
 // alert(3+3) // -> It show Error bcs we are using Node.js, not browser
 
 /*
-Types of DataTypes :-
+Types of DataTypes - 
+(1) Primitive - 7 Types:
     1. Number => 2^53 range
-    2. bigInt
+    2. BigInt
     3. string
     4. boolean => true/false
     5. Null => stand-alone value
     6. undefined => values are not assigned yet
-    7. symbols => unique
+    7. symbol => unique
 
-    Very Imp....
-    1. Object
+(2) Non-Primitive (Reference) - 
+    1. Array
+    2. objects
+    3. Functions
 */
 console.log(typeof undefined); // -> o/p- undefined
 console.log(typeof null); // -> o/p- object
+
+// Symbols-
+const id = Symbol('123');
+const AnotherId = Symbol('123');
+console.log(id === AnotherId);
+
+const heroes = ["shaktiman","naagraj","doga"];       // Array
+
+let myObj = {
+    name:'shamsad',
+    age:24
+}       // Object
+
+const myFunction = function(){
+    console.log("Hello, World");
+}
+myFunction();
