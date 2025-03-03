593c9b352eff0beb0f37e76367583b2840f7a247
SHAMSAD ALAM <77450372+ShamsadAlam@users.noreply.github.com>
2025-03-03 02:35:09 +0000

    Operations defined

--- 1-Basics/3-ConversionOperation.js
+++ 1-Basics/3-ConversionOperation.js
@@ -41,3 +41,20 @@ console.log(booleanIsLoggedIn);
     " " or "Shamsad" ==> true
 
 */
+
+/* ================= Operations =============== */
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " shamsad"
+let str3 = str1+str2;
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1 + 2 + 2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
