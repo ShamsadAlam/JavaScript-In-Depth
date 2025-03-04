1ebb99a7c5e5d4623133e74946f57fc84882c5c1
SHAMSAD ALAM <77450372+ShamsadAlam@users.noreply.github.com>
2025-03-04 02:05:35 +0000

    String Manipulation Done

--- 1-Basics/6-Strings.js
+++ 1-Basics/6-Strings.js
@@ -5,7 +5,7 @@ console.log(name+repoCount+" Value");
 
 console.log(`hello nmy name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('ShamsadAl')
+const gameName = new String('Shamsad-Al-noble')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
@@ -18,3 +18,17 @@ console.log(newString);
 
 const anotherString = gameName.slice(-9,4)
 console.log(anotherString);
+
+const newStringOne = "     Shamsad      ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = 'https://shamsad%20portfolio.vercel.com'
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sham'));
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
