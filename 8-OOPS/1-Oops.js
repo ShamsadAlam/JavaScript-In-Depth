2fdc25ac732198f7c7dca405d8b26cc7ca2c7b73
SHAMSAD ALAM <77450372+ShamsadAlam@users.noreply.github.com>
2025-03-11 14:29:26 +0000

    oops modified

--- 8-OOPS/1-Oops.js
+++ 8-OOPS/1-Oops.js
@@ -12,18 +12,3 @@ const user = {
 // console.log(user.username);
 // console.log(user.getUserDetails());
 // console.log(this);
-
-function User(username, loginCount, isLoggedIn){
-    this.username = username;
-    this.loginCount = loginCount;
-    this.isLoggedIn = isLoggedIn
-    return this;
-}
-const userOne = new User("Shamsad",8, true);
-const userTwo = new User("Alam",10, false);
-console.log(userOne);
-console.log(userTwo);
-/*
-    new Keyword- It always create a new / separate copy of the defined class.
-    If we don't use new keyword in above two lines, userTwo's assigned value will overwrite the userOne that is not good for collaborating environment.
-*/
